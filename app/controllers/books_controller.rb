6ed67c757f04897dfe5d41fb632f20041c0a6f7e
AyaneSuto <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-04-08 11:07:36 +0000

    [Finish] RSpecによるテストクリア

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,11 @@
 class BooksController < ApplicationController
-  
   def create
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to new_book_path(@book.id)
-    else
-      render :new
+      redirect_to book_path(@book.id)
+    else @books = Book.all
+      render :index
     end
   end
 
@@ -36,7 +35,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path(book.id)
+    redirect_to books_path
   end
 
   private
